f43aaf3ac5b082f9f2ff64b2665d56cc2eebcafe
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-12-11 15:58:20 +0530

    project setuped

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // added to the base
+  server: {
+    port: 3000,
+  },
+  define: {
+    global: {}, // Add this line to define `global` as an empty object
+  },
+});
